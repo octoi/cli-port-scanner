c9b801fe50bd7717e0c96756af166f2c32badc9f
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-27 20:50:56 +0530

    multi threaded scanner

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::process;
 use std::sync::mpsc::{Sender, channel};
 use std::thread;
 
+const MAX: u16 = 65535;
+
 struct Arguments {
     flag: String,
     ipaddr: IpAddr,
@@ -70,6 +72,37 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err| {
+            if err.contains("help") {
+                process::exit(0);
+            } else {
+                eprintln!("{} problem parsing arguments: {}", program, err);
+                process::exit(0);
+            }
+        }
+    );
 
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
 
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }

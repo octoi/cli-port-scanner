fa1db66124570a2c34e39f5288d5f3dd17bc6101
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-27 20:36:57 +0530

    args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    for i in &args {
+        println!("{}", i);
+    }
+
+    println!("{}", i);
 }

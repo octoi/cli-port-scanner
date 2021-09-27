8084d2bc094618606322a7dfb001c83f916566c3
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-27 20:50:13 +0530

    scan function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
+use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
 
 struct Arguments {
     flag: String,
@@ -40,6 +44,26 @@ impl Arguments {
                 return Err("invalid syntax");
             }
         }
+    }
+}
+
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+
+        }
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
     }
 }
 
